2d30c72c926b9d1d070b2b64f4db29c93396d7ab
FedericoOcampos <reinalfede@gmail.com>
2025-09-20 16:25:22 -0300

    Url de Api en Web Hardcodeada

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.ENV = {
-  API_URL: "http://mi-api.g4fcevhkeshrh0hn.eastus2.azurecontainer.io:8000"
+  API_URL: "http://tuapp-api-prod-2025.eastus2.azurecontainer.io:8000"
 
 };
